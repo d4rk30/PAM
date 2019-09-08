355d807abb37ed992d68171ab4db538e5753d31c
d4rk30 <d4rk30@nohacker.me>
2019-09-08 21:40:48 +0800

    Optimizing data structure

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for,request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os,sys
 from lib.subdomains import *
+from forms import LoginForm,CreateProjectForm,CreateDomainForm
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -13,8 +15,10 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'secret string'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 #数据库模型
 class User(db.Model):
@@ -29,6 +33,8 @@ class User(db.Model):
 class Project(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     name = db.Column(db.String(120))
+    describe = db.Column(db.String(255))
+    datetime = db.Column(db.DateTime)
     
     #定义外键
     user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
@@ -49,23 +55,25 @@ class Domain(db.Model):
 class Subdomain(db.Model): 
     id = db.Column(db.Integer,primary_key = True)
     url = db.Column(db.String(255))
+    datetime = db.Column(db.DateTime)
     
     #定义外键
     domain_id = db.Column(db.Integer,db.ForeignKey('domain.id'))
 
     #与SubdomainInfo建立关联关系
-    subdomaininfos = db.relationship('SubdomainInfo')
+    subdomain_infos = db.relationship('SubdomainInfo')
 
 class SubdomainInfo(db.Model):
     id = db.Column(db.Integer,primary_key = True)
-    name = db.Column(db.String(255))
-    mail = db.Column(db.String(255))
-    phone = db.Column(db.String(255))
-    city = db.Column(db.String(255))
-    ip = db.Column(db.String(100))
-    country = db.Column(db.String(255))
-    background = db.Column(db.String(255))
-    updatetime = db.Column(db.DateTime)
+    title = db.Column(db.String(255)) #网站名称
+    name = db.Column(db.String(255)) #域名注册人
+    mail = db.Column(db.String(255)) #域名注册邮箱
+    phone = db.Column(db.String(255)) #域名注册手机
+    city = db.Column(db.String(255)) #域名注册城市
+    ip = db.Column(db.String(100)) 
+    country = db.Column(db.String(255)) #IP所属国家
+    background = db.Column(db.String(255)) #后台地址
+    datetime = db.Column(db.DateTime) #更新时间
 
     #定义外键
     subdomain_id = db.Column(db.Integer,db.ForeignKey('subdomain.id'))
@@ -74,13 +82,13 @@ class SubdomainInfo(db.Model):
     ports = db.relationship('Port')
 
     #与WebFingerprint建立关联关系
-    webfingerprints = db.relationship('WebFingerprint')
+    web_fingerprints = db.relationship('WebFingerprint')
 
 class Port(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     port = db.Column(db.Integer)
     server = db.Column(db.String(255))
-    updatetime = db.Column(db.DateTime)
+    datetime = db.Column(db.DateTime)
 
     #定义外键
     subdomain_info_id = db.Column(db.Integer,db.ForeignKey('subdomain_info.id'))
@@ -88,15 +96,20 @@ class Port(db.Model):
 class WebFingerprint(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     info = db.Column(db.String(255))
-    uodatetime = db.Column(db.DateTime)
+    datetime = db.Column(db.DateTime)
 
     #定义外键
     subdomain_info_id = db.Column(db.Integer,db.ForeignKey('subdomain_info.id'))
 
 
 #操作数据库
+def addUser(username = 'Danny'):
+    user = User(username = username)
+    db.session.add(user)
+    db.session.commit()
+
 #创建项目
-def createProject(project_name,username):
+def createProject(project_name='百度',username = 'Danny'):
     project = Project(name = project_name)
     db.session.add(project)
     user = User.query.filter(User.username == username).first_or_404()
@@ -104,7 +117,7 @@ def createProject(project_name,username):
     db.session.commit()
 
 #添加域名
-def addDomain(domain_url,project_name):
+def addDomain(domain_url='baidu.com',project_name="百度"):
     domain = Domain(url = domain_url)
     db.session.add(domain)
     project = Project.query.filter(Project.name == project_name).first_or_404()
@@ -119,17 +132,20 @@ def addSubdomain(subdomain_url,domain_url):
     domain.subdomains.append(subdomain)
     db.session.commit()
 
-#添加域名视图函数
-def addview():
-    addDomain('baidu.com','baidu')
-    subdomains=search('baidu.com')
-    for subdomain in subdomains:
-        addSubdomain(subdomain,'baidu.com')
-
-
-
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    subdomains = Subdomain.query.all()
-    return render_template('index.html',subdomains = subdomains)
+    create_project_form = CreateProjectForm()
+    if request.method == 'POST' and create_project_form.validate():
+        project_name = create_project_form.name.data
+        createProject(project_name = project_name)
+        return redirect(url_for('index'))
+    projects = Project.query.all()
+    return render_template('index.html',create_project_form=create_project_form,projects = projects)
+    
+@app.route('/forms')
+def forms():
+    form = LoginForm()
+    return render_template('test.html',form=form)
+
+    
 
---
+++ forms.py
@@ -0,0 +1,18 @@
+from flask_wtf import FlaskForm
+from wtforms import Form,StringField,PasswordField,BooleanField,SubmitField
+from wtforms.validators import DataRequired,Length
+
+class LoginForm(FlaskForm):
+	username = StringField('Username',validators = [DataRequired()])
+	password = PasswordField('Password',validators = [DataRequired(),Length(8,128)])
+	remember = BooleanField('Remember me')
+	submit = SubmitField('Log in')
+	
+class CreateProjectForm(FlaskForm):
+	name = StringField('Project',validators = [DataRequired()])
+	describe = StringField('Describe',validators = [DataRequired()])
+	submit = SubmitField('Create')
+	
+class CreateDomainForm(FlaskForm):
+	url = StringField('Domain',validators = [DataRequired()])
+	submit = SubmitField('Create')
